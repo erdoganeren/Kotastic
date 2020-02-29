8f770a4d10173cbf7789522c0512427fc39642e2
philipp <philipp@Philipps-MacBook.local>
2020-02-29 13:13:44 +0100

    added dogBagPoint

---
+++ static/js/dogBagPoint.js
@@ -0,0 +1,39 @@
+class DocBagPoint {
+
+    constructor(){}
+
+    get id() {
+        return this.id;
+    }
+    set id(x) {
+        this.id = x;
+    }
+
+    get longitude() {
+        return this.longitude;
+    }
+    set longitude(x) {
+        this.longitude = x;
+    }
+
+    get latitude() {
+        return this.latitude;
+    }
+    set latitude(x) {
+        this.latitude = x;
+    }
+
+    get streetname() {
+        return this.streetname;
+    }
+    set streetname(x) {
+        this.streetname = x;
+    }
+
+    get number() {
+        return this.number;
+    }
+    set number(x) {
+        this.number = x;
+    }
+}
