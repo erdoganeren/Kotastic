966d53e30f870a6d0d08e73fb50db628930690e1
philipp <philipp@Philipps-MacBook.local>
2020-02-29 12:10:03 +0100

    added json file

--- main/pacGui/StartGUI.java
+++ main/pacGui/StartGUI.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartGUI {
 	private final static int servicePort = 8181;
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	    logger.setLevel(Level.ALL);
 		SpringApplication app = new SpringApplication(StartGUI.class);
